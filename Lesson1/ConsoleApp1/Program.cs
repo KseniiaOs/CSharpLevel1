fb7c942322e03b162d05bcbd5342f3cbd741e387
Kseniia Osokina <ksenia.osokina@gmail.com>
2019-02-21 17:40:07 +0300

    questionnaire release

---
+++ Lesson1/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionaryL1
+{
+    class Programm
+
+    {
+        static void Main()
+        {
+            Console.WriteLine("First Name:");
+            string fn = Console.ReadLine();
+
+            Console.WriteLine("Second Name:");
+            string sn = Console.ReadLine();
+
+            Console.WriteLine("Your Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Height:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Weight:");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(fn + " " + sn + " " + age + " " + height + " " + weight);
+            Console.ReadKey();
+
+        }
+    }
+
+}
+
+
+
+namespace QuestionaryL2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("First Name:");
+            string fn = Console.ReadLine();
+            Console.WriteLine("Second Name:");
+            string sn = Console.ReadLine();
+            Console.WriteLine("Your Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Height:");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Weight:");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(String.Format("Your applicationa was accepted as {0} {1} {2} {3} {4}", fn, sn, age, height, weight));
+            Console.ReadKey();
+
+        }
+
+    }
+
+
+
+
+    namespace QuestionaryL3
+{ 
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("First Name:");
+            string fn = Console.ReadLine();
+            Console.WriteLine("Second Name:");
+            string sn = Console.ReadLine();
+            Console.WriteLine("Your Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Height:");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Weight:");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Your applicationa was accepted as {fn} {sn} {age} {height} {weight}");
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
+
