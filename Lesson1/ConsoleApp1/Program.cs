8160461f861a59b342599a78816b53266c0a1ae5
Kseniia Osokina <ksenia.osokina@gmail.com>
2019-02-21 17:45:57 +0300

    second

--- Lesson1/ConsoleApp1/Program.cs
+++ Lesson1/ConsoleApp1/Program.cs
@@ -4,61 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuestionaryL1
-{
-    class Programm
+//namespace QuestionaryL1
+//{
+//    class Programm
 
-    {
-        static void Main()
-        {
-            Console.WriteLine("First Name:");
-            string fn = Console.ReadLine();
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("First Name:");
+//            string fn = Console.ReadLine();
 
-            Console.WriteLine("Second Name:");
-            string sn = Console.ReadLine();
+//            Console.WriteLine("Second Name:");
+//            string sn = Console.ReadLine();
 
-            Console.WriteLine("Your Age:");
-            int age = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Your Age:");
+//            int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Height:");
-            double height = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine("Height:");
+//            double height = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Weight:");
-            double weight = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine("Weight:");
+//            double weight = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(fn + " " + sn + " " + age + " " + height + " " + weight);
-            Console.ReadKey();
+//            Console.WriteLine(fn + " " + sn + " " + age + " " + height + " " + weight);
+//            Console.ReadKey();
 
-        }
-    }
+//        }
+//    }
 
-}
+//}
 
 
 
-namespace QuestionaryL2
-{
-    public class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("First Name:");
-            string fn = Console.ReadLine();
-            Console.WriteLine("Second Name:");
-            string sn = Console.ReadLine();
-            Console.WriteLine("Your Age:");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Height:");
-            double height = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Weight:");
-            double weight = Convert.ToDouble(Console.ReadLine());
+//namespace QuestionaryL2
+//{
+//    public class Program
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("First Name:");
+//            string fn = Console.ReadLine();
+//            Console.WriteLine("Second Name:");
+//            string sn = Console.ReadLine();
+//            Console.WriteLine("Your Age:");
+//            int age = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Height:");
+//            double height = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine("Weight:");
+//            double weight = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(String.Format("Your applicationa was accepted as {0} {1} {2} {3} {4}", fn, sn, age, height, weight));
-            Console.ReadKey();
+//            Console.WriteLine(String.Format("Your applicationa was accepted as {0} {1} {2} {3} {4}", fn, sn, age, height, weight));
+//            Console.ReadKey();
 
-        }
+//        }
 
-    }
+//    }
 
 
 
